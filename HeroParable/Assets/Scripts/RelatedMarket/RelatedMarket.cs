83637557bd4dc6d69e9d67595a07b76e02be5b96
kimdh9852 <kimdh9852@users.noreply.github.com>
2019-02-25 15:27:59 +0900

    new 2019.02.25

--- HeroParable/Assets/Scripts/RelatedMarket/RelatedMarket.cs
+++ HeroParable/Assets/Scripts/RelatedMarket/RelatedMarket.cs
@@ -62,7 +62,7 @@ public class RelatedMarket : MonoBehaviour {
 
     public void RandomBox(int item_Money)
     {
-        int rand;
+        int random;
 
         if (Money < item_Money)
         {
@@ -72,46 +72,53 @@ public class RelatedMarket : MonoBehaviour {
 
         Money -= item_Money;
 
+        common = 0;
+        uncommon = 0;
+        rare = 0;
+        uniq = 0;
+        legend = 0;
+
         for (int i = 0; i < 10; i++)
         {
-            rand = Random.Range(0, 10000);
+            random = Random.Range(1, 10000);
 
             SuccessBuy.gameObject.SetActive(true);
-            if (rand < 5000)
+            if (random < 5001)//5000
             {
                 common++;
                 total++;
-                SuccessBuy.gameObject.transform.GetChild(0).GetComponent<Text>().text = "커먼템 x " + common.ToString()+"획득";
             }
-            else if (rand < 7000)
+            else if (random < 8001)//3000
             {
                 uncommon++; 
                 total++;
-                SuccessBuy.gameObject.transform.GetChild(1).GetComponent<Text>().text = "언커먼템 x " + uncommon.ToString() + "획득";
             }
-            else if (rand < 9000)
+            else if (random < 9501)//1500
             {
                 rare++; 
                 total++;
-                SuccessBuy.gameObject.transform.GetChild(2).GetComponent<Text>().text = "레어템 x " + rare.ToString() + "획득";
             }
-            else if (rand < 9999)
+            else if (random < 9999)//499
             {
                 uniq++; 
                 total++;
-                SuccessBuy.gameObject.transform.GetChild(3).GetComponent<Text>().text = "유니크템 x " + uniq.ToString() + "획득";
             }
-            else
+            else //1
             {
                 legend++;
-                total_legend++;
                 total++;
-                SuccessBuy.gameObject.transform.GetChild(4).GetComponent<Text>().text = "레전드템 x " + legend.ToString() + "획득";
             }
         }
+
+        SuccessBuy.gameObject.transform.GetChild(0).GetComponent<Text>().text = "커먼템 x " + common.ToString() + "획득";
+        SuccessBuy.gameObject.transform.GetChild(1).GetComponent<Text>().text = "언커먼템 x " + uncommon.ToString() + "획득";
+        SuccessBuy.gameObject.transform.GetChild(2).GetComponent<Text>().text = "레어템 x " + rare.ToString() + "획득";
+        SuccessBuy.gameObject.transform.GetChild(3).GetComponent<Text>().text = "유니크템 x " + uniq.ToString() + "획득";
+        SuccessBuy.gameObject.transform.GetChild(4).GetComponent<Text>().text = "레전드템 x " + legend.ToString() + "획득";
+
         //Debug.Log(" common : " + common + " uncommon : " + uncommon + " rare : "
         //           + rare + " uniq : " + uniq + " legend : " + legend + " Total : " + total + " total_legend : " + total_legend);
-        common = 0; uncommon = 0; rare = 0; uniq = 0; legend = 0;
+
     }
     //public Rect windowSize = new Rect(15, 15, 250, 250);
 
