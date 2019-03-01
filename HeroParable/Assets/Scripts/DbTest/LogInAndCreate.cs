a0b87cf849ac8dc777a0a82ff575a8af0d252818
kimdh9852 <46774291+kimdh9852@users.noreply.github.com>
2019-03-01 14:51:15 +0900

    로그인 추가
    f

---
+++ HeroParable/Assets/Scripts/DbTest/LogInAndCreate.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogInAndCreate : MonoBehaviour
+{
+    [Header("Login")]
+    public InputField Id;
+    public InputField Password;
+    public string userid;
+    public string userpassword;
+    public string username;
+
+    [Header("CreateId")]
+    public GameObject CreatePanel;
+    public InputField C_id;
+    public InputField C_PassWord;
+    public InputField C_Name;
+    public string CU_userid;
+    public string CU_userpassword;
+    public string CU_username;
+
+    public GameObject FailedPanel;
+    
+    void Update()
+    {
+        userid = Id.text;
+        userpassword = Password.text;
+        CU_userid = C_id.text;
+        CU_userpassword = C_PassWord.text;
+        CU_username = C_Name.text;
+    }
+
+    public void Login()
+    {
+        StartCoroutine(Co_login());
+    }
+    
+    public void CreateUser()
+    {
+        StartCoroutine(Co_CreateUser());
+    }
+
+    IEnumerator Co_login()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("Id", userid);
+        form.AddField("PassWord", userpassword);
+
+        WWW www = new WWW("http://localhost/heroparable/login.php", form);
+
+        yield return www;
+
+        if (www.text == "OK")
+        {
+            GameObject.Find("SceneMover").GetComponent<SceneMove>().GoLobby();
+        }
+        else
+        {
+            Debug.Log(www.text);
+
+            FailedPanel.SetActive(true);
+
+            if (www.text == "ID")
+            {
+                FailedPanel.transform.GetChild(0).GetComponent<Text>().text = "존재하지 않는 ID입니다.";
+            }
+            else
+            {
+                FailedPanel.transform.GetChild(0).GetComponent<Text>().text = "잘못된 PassWord입니다.";
+            }
+        }
+    }
+
+    IEnumerator Co_CreateUser()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("Id", CU_userid);
+        form.AddField("PassWord", CU_userpassword);
+        form.AddField("Name", CU_username);
+
+        WWW www = new WWW("http://localhost/heroparable/createid.php", form);
+
+        yield return www;
+
+        if (www.text == "OK")
+        {
+            GameObject.Find("SceneMover").GetComponent<SceneMove>().GoLobby();
+        }
+        else
+        {
+            Debug.Log(www.text);
+
+            FailedPanel.SetActive(true);
+
+            if (www.text == "ID")
+            {
+                FailedPanel.transform.GetChild(0).GetComponent<Text>().text = "존재하는 ID입니다.";
+            }
+            else
+            {
+                FailedPanel.transform.GetChild(0).GetComponent<Text>().text = "존재하는 Name입니다.";
+            }
+        }
+    }
+
+    public void Confirm(string panname)
+    {
+        if (panname == "fail")
+        {
+            FailedPanel.SetActive(false);
+        }
+        else
+        {
+            CreatePanel.SetActive(false);
+        }
+    }
+    
+    public void OnCreatePanel()
+    {
+        CreatePanel.SetActive(true);
+    }
+}
---
+++ HeroParable/Assets/Scripts/DbTest/Readstat.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Readstat : MonoBehaviour
+{
+    public Text Hp;
+    public int hp;
+    public Text AP;
+    public int ap;
+    public Text AR;
+    public float ar;
+    public Text MS;
+    public float ms;
+    public Text Sp;
+
+    public int Statpoint = 5;
+
+    public string[] stats;
+
+    //스탯을 db에서 불러오는 코드
+    IEnumerator Start()
+    {
+        //db주소 받아오는 코드
+        WWW ReadStatData = new WWW("http://localhost/heroparable/readstat.php");
+        //받아올때까지 대기
+        yield return ReadStatData;
+
+        string RSDString = ReadStatData.text;
+        //받아온 정보들은 한줄의 긴 문장이기때문에|를 기준으로 분리하여 배열에 저장
+        stats = RSDString.Split('|');
+
+        for (int i = 0; i < stats.Length; i++)
+        {
+            //정보의 끝값에는 ;가 붙어있기때문에 이를 제거하는 작업을 거침
+            stats[i] = GetDataValue(stats[i]);
+        }
+        //string정보들을 각 스탯 형식에 맞도록 파싱
+        Parshing_Stats();
+    }
+
+    public void Parshing_Stats()
+    {
+        hp = int.Parse(stats[0]);
+        ap = int.Parse(stats[1]);
+        ar = float.Parse(stats[2]);
+        ms = float.Parse(stats[3]);       
+    }
+    
+    string GetDataValue(string data)
+    {
+        string value = data;
+
+        if (value.Contains(";"))
+            value = value.Remove(value.IndexOf(";"));
+        
+        return value;
+    }
+    //스탯을 디비에 업데이트하는 코드
+    public void Statconfirm()
+    {
+        //php파일에는 변수들을 POST하기위하여 form에 값들을 저장
+        //float값을 전달 안됨 ->ToString()으로 해결됨을 확인
+        WWWForm form = new WWWForm();
+        form.AddField("Hp", hp);
+        form.AddField("AttackPower", ap);
+        form.AddField("AttackRange", ar.ToString());
+        form.AddField("MoveSpeed", ms.ToString());
+        //디비 주소에다가 POST값을 전달. 수정은 php파일 내에서 이뤄짐
+        WWW www = new WWW("http://localhost/heroparable/updatestat.php", form);
+    }
+
+    public void StatUp(string stat)
+    {
+        if (Statpoint == 0)
+            return;
+
+        Statpoint--;
+
+        switch (stat)
+        {
+            case "Hp":
+                hp += 100;
+                break;
+            case "Ap":
+                ap += 100;
+                break;
+            case "Ar":
+                ar += 0.5f;
+                break;
+            case "Ms":
+                ms += 0.5f;
+                break;
+        }
+    }
+
+    public void StatDown(string stat)
+    {
+        if (Statpoint == 5 )
+            return;
+
+        Statpoint++;
+
+        switch (stat)
+        {
+            case "Hp":
+                hp -= 100;
+                break;
+            case "Ap":
+                ap -= 100;
+                break;
+            case "Ar":
+                ar -= 0.5f;
+                break;
+            case "Ms":
+                ms -= 0.5f;
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        Hp.text = hp.ToString();
+        AP.text = ap.ToString();
+        AR.text = ar.ToString();
+        MS.text = ms.ToString();
+        Sp.text = Statpoint.ToString();
+    }
+
+}
