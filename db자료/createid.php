47cd394bee523c6ba5f586481f24d135b5cd3a94
kimdh9852 <kimdh9852@users.noreply.github.com>
2019-03-05 15:12:02 +0900

    123
    123

--- db자료/createid.php
+++ db자료/createid.php
@@ -7,7 +7,6 @@
 
 	$id = $_POST['Id'];
 	$password = $_POST['PassWord'];
-	$name = $_POST['Name'];
 	
 	$con = new mysqli($servername,$server_username,$server_password,$dbname);
 
@@ -15,26 +14,10 @@
 		die("Connection Failed. ".mysqli_connect_error);
 	}
 	
-	$id_sql = mysqli_query($con,"SELECT * FROM userlist WHERE Id = '".$id."'"); 
-	$name_sql = mysqli_query($con,"SELECT * FROM userlist WHERE Name = '".$name."'"); 
-	$id_overlap_decision = mysqli_fetch_array($id_sql); 
-	$name_overlap_decision = mysqli_fetch_array($name_sql); 
-
-	if($id_overlap_decision != 0) 
-	{
-		echo ("Id");
-		die();
-	}
-	else if($name_overlap_decision != 0) 
-	{	
-		echo ("Name");
-		die();
-	}
-	
-	$sql = "INSERT INTO userlist(Id,PassWord,Name) VALUES ('".$id."','".$password."','".$name."')"; 
+	$sql = "INSERT INTO userlist(Id,PassWord) VALUES ('".$id."','".$password."')"; 
 	$result = mysqli_query($con,$sql);
 	
-	if(!$result) echo "Failed";
+	if(!$result) echo "Id";
 		else echo "OK";
 	
 ?>
